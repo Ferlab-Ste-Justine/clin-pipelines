d11272a30d9d938afdc1dca0ecea8bec098091bd
evans-g-crsj <eg.crsj@gmail.com>
2021-11-25 17:30:03 -0500

    feature(email notification): clin-394 - first draft - add response extraction

--- src/main/scala/bio/ferlab/clin/etl/task/ldmnotifier/TasksGqlExtractor.scala
+++ src/main/scala/bio/ferlab/clin/etl/task/ldmnotifier/TasksGqlExtractor.scala
@@ -1,10 +1,53 @@
 package bio.ferlab.clin.etl.task.ldmnotifier
 
-import bio.ferlab.clin.etl.task.ldmnotifier.model.Task
+import bio.ferlab.clin.etl.task.ldmnotifier.model.{GqlResponse, Task}
 import play.api.libs.json._
 
 object TasksGqlExtractor {
-  def extractTasks(json: JsValue): Either[String, Seq[Task]] = {
+  def buildGqlTasksQueryHttpPostBody(runName: String): String = {
+    val query = s"""
+                 |{
+                 |	taskList: TaskList(run_name: "$runName") {
+                 |		id
+                 |	    owner @flatten {
+                 |		  owner: resource(type: Organization) {
+                 |				id
+                 |				alias @first @singleton
+                 |			}
+                 |		}
+                 |		 output @flatten {
+                 |			valueReference @flatten {
+                 |				attachments: resource(type: DocumentReference) {
+                 |                     content @flatten {
+                 |						 urls: attachment {
+                 |							url
+                 |						}
+                 |					}
+                 |				}
+                 |			}
+                 |		}
+                 |	}
+                 |}
+                 |
+                 |""".stripMargin
+
+    s"""{ "query": ${JsString(query)} }"""
+  }
+
+  def checkIfGqlResponseHasData(json: JsValue): Either[String, Boolean] = {
+    (json).validate[GqlResponse].fold(
+      invalid => {
+        println("invalid", invalid)
+        Left(s"Errors: ${JsError.toJson(invalid)}")
+      },
+      valid => {
+        val data = valid.data
+        Right(data.isDefined && data.get.taskList.isDefined)
+      },
+    )
+  }
+
+  def extractTasksWhenHasData(json: JsValue): Either[String, Seq[Task]] = {
     (json \ "data" \ "taskList").validate[Seq[Task]].fold(
       invalid => {
         Left(s"Errors: ${JsError.toJson(invalid)}")
--- src/test/scala/bio/ferlab/clin/etl/fhir/TasksGqlExtractorSpec.scala
+++ src/test/scala/bio/ferlab/clin/etl/fhir/TasksGqlExtractorSpec.scala
@@ -6,16 +6,41 @@ import org.scalatest.Matchers.convertToAnyShouldWrapper
 import org.scalatest.{FlatSpec, GivenWhenThen}
 
 class TasksGqlExtractorSpec extends FlatSpec with GivenWhenThen {
+  "buildGqlTasksQueryHttpPostBody" should "give a correct http post body" in {
+    Given("a runName 'abc' ")
+    val runName = "abc"
+    Then("a correct graphql query should be produced")
+    val httpBody = TasksGqlExtractor.buildGqlTasksQueryHttpPostBody("abc")
+    println(httpBody)
+    httpBody shouldBe "{ \"query\": \"\\n{\\n\\ttaskList: TaskList(run_name: \\\"abc\\\") {\\n\\t\\tid\\n\\t    owner @flatten {\\n\\t\\t  owner: resource(type: Organization) {\\n\\t\\t\\t\\tid\\n\\t\\t\\t\\talias @first @singleton\\n\\t\\t\\t}\\n\\t\\t}\\n\\t\\t output @flatten {\\n\\t\\t\\tvalueReference @flatten {\\n\\t\\t\\t\\tattachments: resource(type: DocumentReference) {\\n                     content @flatten {\\n\\t\\t\\t\\t\\t\\t urls: attachment {\\n\\t\\t\\t\\t\\t\\t\\turl\\n\\t\\t\\t\\t\\t\\t}\\n\\t\\t\\t\\t\\t}\\n\\t\\t\\t\\t}\\n\\t\\t\\t}\\n\\t\\t}\\n\\t}\\n}\\n\\n\" }"
+  }
+
+  "A well-formed graphql response with no data" should "be handled correctly" in {
+    Given("a correctly parsed json containing no data ({ 'data': {} })")
+    val parsed = FhirTestUtils.parseJsonFromResource("task/graphql_http_resp_empty_data_no_errors.json")
+    val eitherErrorOrData = TasksGqlExtractor.checkIfGqlResponseHasData(parsed.get)
+    Then("no error is reported")
+    val hasError = eitherErrorOrData.isLeft
+    hasError shouldBe false
+    And("reports that there is no data, as well")
+    val hasData =  eitherErrorOrData.right.get
+    hasData shouldBe false
+  }
+
   "A well-formed graphql response with data" should "give task(s)" in {
     Given("a correctly parsed json containing 4 tasks")
     val parsed = FhirTestUtils.parseJsonFromResource("task/graphql_http_resp_run_name_1.json")
 
     Then("the tasks should be extracted with no errors")
-    val either = TasksGqlExtractor.extractTasks(parsed.get)
-    either.isLeft shouldBe false
+    val eitherErrorOrData = TasksGqlExtractor.checkIfGqlResponseHasData(parsed.get)
+    eitherErrorOrData.isLeft shouldBe false
+    eitherErrorOrData.isRight shouldBe true
+
+    val eitherErrorOrTasks = TasksGqlExtractor.extractTasksWhenHasData(parsed.get)
+    eitherErrorOrTasks.isLeft shouldBe false
 
     And("their size be the same as in the raw data")
-    val tasks = either.right.get
+    val tasks = eitherErrorOrTasks.right.get
     tasks.size shouldBe 4
   }
 }
