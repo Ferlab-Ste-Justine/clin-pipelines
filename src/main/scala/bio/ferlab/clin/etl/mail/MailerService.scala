5c3bf366d1ea208c31688061d6bcc075019c31ca
evans-g-crsj <eg.crsj@gmail.com>
2021-12-01 17:01:21 -0500

    add support for email

--- src/main/scala/bio/ferlab/clin/etl/mail/MailerService.scala
+++ src/main/scala/bio/ferlab/clin/etl/mail/MailerService.scala
@@ -1,39 +1,20 @@
 package bio.ferlab.clin.etl.mail
 
-import bio.ferlab.clin.etl.conf.Conf
 import play.api.libs.mailer._
-
 import javax.inject.Inject
 
-case class EmailParams(
-                        to: String,
-                        from: String,
-                        bccs: Seq[String],
-                        subject: String,
-                        bodyText: String,
-                        attachments: Seq[AttachmentFile]
-                      )
-
-class MailerService @Inject()(mailerClient: MailerClient) {
+class MailerService @Inject() (mailerClient: MailerClient) {
   private type MessageId = String
 
-  def sendEmail(params: EmailParams): MessageId = {
+  def sendEmail(to: String, bodyText: String, attachments: Seq[AttachmentFile]): MessageId = {
     val email = Email(
-      params.subject,
-      params.from,
-      Seq(params.to),
-      attachments = params.attachments,
-      bodyText = Some(params.bodyText),
-      bcc = params.bccs
+      "subject",
+      "Mister FROM <from@email.com>",
+      Seq(to),
+      attachments = attachments,
+      bodyText = Some(bodyText),
     )
     mailerClient.send(email)
   }
-}
 
-object MailerService {
-  def makeSmtpMailer(conf: Conf) = new SMTPMailer(SMTPConfiguration(conf.mailer.host, conf.mailer.port))
-
-  def adjustBccType(conf: Conf): Seq[String] =
-    if (conf.mailer.bcc.isBlank) Seq.empty
-    else conf.mailer.bcc.split(",").map(_.trim).toSeq
 }
