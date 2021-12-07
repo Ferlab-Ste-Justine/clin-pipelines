d63e346eace789f4999f67b58cd5b967256079cb
jecos <jcostanza@ferlab.bio>
2021-12-06 23:30:03 -0500

    fix: CLIN-394 Refactoring and fix tests

--- src/main/scala/bio/ferlab/clin/etl/task/ldmnotifier/TasksGqlExtractor.scala
+++ src/main/scala/bio/ferlab/clin/etl/task/ldmnotifier/TasksGqlExtractor.scala
@@ -1,6 +1,6 @@
 package bio.ferlab.clin.etl.task.ldmnotifier
 
-import bio.ferlab.clin.etl.task.ldmnotifier.model.{GqlResponse, Task}
+import bio.ferlab.clin.etl.task.ldmnotifier.model.{Data, GqlResponse, Task}
 import play.api.libs.json._
 import sttp.client3.{HttpURLConnectionBackend, Identity, Response, UriContext, basicRequest}
 import sttp.model.MediaType
@@ -11,34 +11,35 @@ object TasksGqlExtractor {
   val GQL_COUNT_UPPER_BOUND = 1000 //needed right now to make graphql queries work.
 
   def buildGqlTasksQueryHttpPostBody(runName: String): String = {
-    val query = s"""
-                 |{
-                 |	taskList: TaskList(run_name: "$runName") {
-                 |		id
-                 |	    owner @flatten {
-                 |		  owner: resource(type: Organization) {
-                 |				id
-                 |				alias @first @singleton
-                 |        telecom @flatten @first @singleton {
-                 |            email: value
-                 |          }
-                 |			}
-                 |		}
-                 |		 output @flatten {
-                 |			valueReference @flatten {
-                 |				attachments: resource(type: DocumentReference) {
-                 |                     content @flatten {
-                 |						 urls: attachment {
-                 |							url
-                 |						}
-                 |					}
-                 |				}
-                 |			}
-                 |		}
-                 |	}
-                 |}
-                 |
-                 |""".stripMargin
+    val query =
+      s"""
+         |{
+         |	taskList: TaskList(run_name: "$runName") {
+         |		id
+         |	    owner @flatten {
+         |		  owner: resource(type: Organization) {
+         |				id
+         |				alias @first @singleton
+         |        telecom @flatten @first @singleton {
+         |            email: value
+         |          }
+         |			}
+         |		}
+         |		 output @flatten {
+         |			valueReference @flatten {
+         |				attachments: resource(type: DocumentReference) {
+         |                     content @flatten {
+         |						 urls: attachment {
+         |							url
+         |						}
+         |					}
+         |				}
+         |			}
+         |		}
+         |	}
+         |}
+         |
+         |""".stripMargin
     s"""{ "query": ${JsString(query)} }"""
   }
 
@@ -54,26 +55,20 @@ object TasksGqlExtractor {
     response
   }
 
-  def checkIfGqlResponseHasData(json: JsValue): Either[String, Boolean] = {
-    (json).validate[GqlResponse].fold(
+  def checkIfGqlResponseHasData(json: JsValue): Either[String, Seq[Task]] = {
+    json.validate[GqlResponse].fold(
       invalid => {
         Left(s"Errors: ${JsError.toJson(invalid)}")
       },
       valid => {
-        val data = valid.data
-        Right(data.isDefined && data.get.taskList.isDefined)
+        valid.data match {
+          case Some(Data(Some(taskList))) if taskList.nonEmpty => Right(taskList)
+          case Some(_) => Left("Task list is empty")
+          case _ => Left(s"Errors: No data for task")
+        }
       },
     )
   }
 
-  def extractTasksWhenHasData(json: JsValue): Either[String, Seq[Task]] = {
-    (json \ "data" \ "taskList").validate[Seq[Task]].fold(
-      invalid => {
-        Left(s"Errors: ${JsError.toJson(invalid)}")
-      },
-      valid => {
-        Right(valid)
-      },
-    )
-  }
+
 }
